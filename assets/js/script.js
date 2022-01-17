25bf929af8533d76323c5812be6cb03ecf270186
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-01-17 13:22:36 +0100

    timeline

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,36 @@
 const circle1 = new mojs.Shape({
-    parent: '.div-animated',
     shape: 'circle',
-    fill: 'blue',
-    x: 500,
-}).play();
+    fill: {'red': 'blue'},
+    x: {0: 100},
+    duration: 1000,
+}).then({
+    fill: {'blue': 'yellow'},
+    x: {100:200},
+});
+
+
 const circle2 = new mojs.Shape({
-    parent: '.div-animated',
     shape: 'circle',
-    fill: 'yellow',
-    x: 400,
-}).play();
-const circle3 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'green',
-    x: 300,
-}).play();
-const circle4 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'red',
-    x: 200,
-}).play();
-const circle5 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'gray',
-    x: 100,
-}).play();
-const circle6 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'purple',
-}).play();
+    fill: {'yellow': 'blue'},
+    x: {0: -100},
+    duration: 1000,
+}).then({
+    fill: {'blue': 'red'},
+    x: {0: -200},
+});
 
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    // new MojsPlayer({ add: circle1, circle2, isPlaying: true, isRepeat: true});
+const maximeTimeline = new mojs.Timeline({});
+maximeTimeline.add(circle1, circle2);
+// create the timeline
+const mainTimeline = new mojs.Timeline({});
+mainTimeline.add(maximeTimeline);
+
+// add the timeline to the player
+const mojsPlayer = new MojsPlayer({
+  add: mainTimeline
+});
 })
 
