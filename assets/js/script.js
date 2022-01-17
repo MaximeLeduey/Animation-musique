3348b200ace90552bdad3984b4980d1e5a6a3cc3
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-01-17 12:01:34 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,42 @@
-const circle = new mojs.Shape({
-    parent: '.one',
+const circle1 = new mojs.Shape({
+    parent: '.div-animated',
     shape: 'circle',
-    radius: {20:50},
-    fill: {'red': 'blue'},
-    duration: 2000,
-    isYoyo: true,
-    repeat: 1,
-    isShowStart: true,
-}).then({
-   x: 'rand(-250,250)',
+    fill: 'blue',
+    x: 500,
+}).play();
+const circle2 = new mojs.Shape({
+    parent: '.div-animated',
+    shape: 'circle',
+    fill: 'yellow',
+    x: 400,
+}).play();
+const circle3 = new mojs.Shape({
+    parent: '.div-animated',
+    shape: 'circle',
+    fill: 'green',
+    x: 300,
+}).play();
+const circle4 = new mojs.Shape({
+    parent: '.div-animated',
+    shape: 'circle',
+    fill: 'red',
+    x: 200,
+}).play();
+const circle5 = new mojs.Shape({
+    parent: '.div-animated',
+    shape: 'circle',
+    fill: 'gray',
+    x: 100,
+}).play();
+const circle6 = new mojs.Shape({
+    parent: '.div-animated',
+    shape: 'circle',
+    fill: 'purple',
 }).play();
 
 
 
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
-    new MojsPlayer({ add: circle, isPlaying: true, isRepeat: true});
+    // new MojsPlayer({ add: circle1, circle2, isPlaying: true, isRepeat: true});
 })
 
