cc1121f6ce1c4e7d7d7063e3e34de91ca8ede2af
Evan ROZEN <evanrozen3@gmail.com>
2022-01-17 09:46:22 +0100

    toto

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,5 @@ new MojsPlayer({ add: , isPlaying: true, isRepeat: true});
 document.addEventListener('DOMContentLoaded', (event) => {
     bouncyCircle.play();
 })
+
+let toto = "maxime l 'unique"
