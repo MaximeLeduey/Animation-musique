0a6aed6900258662f58208a7c6fb49248f07b848
Evan ROZEN <evanrozen3@gmail.com>
2022-01-18 13:14:59 +0100

    Timeline evan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,7 @@
-<<<<<<< HEAD
 /* -------------------------------------------------------------------------------------------
-----------------------------Début timeline de Evan--------------------------------------------
----------------------------------------------------------------------------------------------- */
+----------------------------Début timeline de Evan-------------------------------------------- */
 
 
-=======
-let music = new Audio('/assets/song/futur-bass.mp3');
-let bpm = 60/133;
-bpm = bpm * 1000;
-
-
-const circle1 = new mojs.Shape({
-    shape: 'circle',
-    stroke: 'purple',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    isShowEnd: false,
-    duration: bpm,
-});
-
-
-const circle2 = new mojs.Shape({
-    shape: 'polygon',
-    isShowEnd: false,
-    stroke: 'indigo',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    duration: bpm,
-    x: -400,
-    y: -150,
-});
-
-const circle3 = new mojs.Shape({
-    shape: 'rect',
-    isShowEnd: false,
-    stroke: 'blue',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    duration: bpm,
-    x: 200,
-    y: 100,
-});
-
-
-const circle4 = new mojs.Shape({
-    shape: 'curve',
-    stroke: 'green',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    isShowEnd: false,
-    duration: bpm,
-    x: -100,
-    y: -100,
-});
-
-const circle5 = new mojs.Shape({
-    shape: 'circle',
-    stroke: 'yellow',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    isShowEnd: false,
-    duration: bpm,
-    x: 400,
-    y: 100,
-});
->>>>>>> c59ec3fa8ed92ddb7f649cbd4ce95b8ecb91964a
 
 // const circle1 = new mojs.Shape({
 //   parent: ".div-animated",
@@ -83,7 +15,6 @@ const circle5 = new mojs.Shape({
 //   speed: 1,
 // }).play();
 
-<<<<<<< HEAD
 const rect1 = new mojs.Shape({
   parent: ".div-animated",
   shape: "rect",
@@ -145,67 +76,6 @@ const star1 = new mojs.Shape({
   y: { 0: 200 },
   speed: 3,
   delay: 900,
-=======
-const circle6 = new mojs.Shape({
-    shape: 'curve',
-    stroke: 'orange',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    isShowEnd: false,
-    duration: bpm,
-    x: -400,
-});
-
-const circle7 = new mojs.Shape({
-    shape: 'circle',
-    stroke: 'red',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    isShowEnd: false,
-    duration: bpm,
-    x: 400,
-});
-
-const circle8 = new mojs.Shape({
-    shape: 'rect',
-    stroke: 'red',
-    scale:    { 0 : 1 },
-    strokeWidth: { 50 : 0 },
-    fill: 'transparent',
-    isShowEnd: false,
-    duration: bpm,
-    y: -150,
-});
-
-const restart = new mojs.Shape({
-    shape: 'rect',
-    fill: 'transparent',
-    scale: {0:1},
-    duration: bpm,
-})
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-// timeline de maxime
-const maximeTimeline = new mojs.Timeline({});
-maximeTimeline.append(circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, );
-// timeline principale
-const mainTimeline = new mojs.Timeline({repeat:8});
-mainTimeline.add(maximeTimeline);
-
-let play = document.querySelector('.div-animated');
-play.addEventListener('click', function(){
-    mainTimeline.play();
-    music.play();
-})
-
-// on ajoute la timeline principale au player
-// const mojsPlayer = new MojsPlayer({
-//   add: mainTimeline
-// });
->>>>>>> c59ec3fa8ed92ddb7f649cbd4ce95b8ecb91964a
 })
   .then({
     fill: "red",
@@ -268,7 +138,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     isRepeat: true,
   });
 });
-
 /* -------------------------------------------------------------------------------------------
 ----------------------------Fin timeline de Evan--------------------------------------------
 ---------------------------------------------------------------------------------------------- */
