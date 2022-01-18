56255a097385848564c0b8c69dbb86a8c3a1a5d1
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-01-18 13:03:31 +0100

    burst

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,82 @@
-const circle = new mojs.Shape({
-    parent: '.one',
-    shape: 'circle',
-    radius: {20:50},
-    fill: {'red': 'blue'},
-    duration: 2000,
-    isYoyo: true,
-    repeat: 1,
-    isShowStart: true,
-}).then({
-   x: 'rand(-250,250)',
-}).play();
+/* -----------------------------------------------------
+timeline début de Yanis 
+------------------------------------------------------- */
+
+
+
+
+const COLORS = {
+    white:   '#ffffff',
+    black:   '#000000',
+    green:   '#49F2CC',
+    lightGrey: '#777',
+    grey:    '#29363B',
+    cyan:    'cyan',
+    yellow:  '#FFE202',
+    hotpink: 'deeppink',
+    red: '#F05454',
+    beige: '#30475E',
+    blue : '#562f7e'
+  };
+
+
+/* const carre = new mojs.Shape({
+  shape:    'rect', 
+  fill:     'none',
+  stroke:   'purple',
+  scale:    { 0 : 1 },
+  strokeWidth: { 50 : 0 },
+  y:         -20,
+  duration:  600,
+  delay: 50,
+  radius: 200,
+  top : 370,
+});
+
+const carre1 = new mojs.Shape({
+  shape:    'rect', 
+  fill:     'none',
+  stroke:   'purple',
+  scale:    { 1 : 0 },
+  strokeWidth: { 50 : 0 },
+  y:         -10,
+  duration:  600,
+  radius: 200,
+  top: 370,
+}); */
+
+
+
+  const burst1 = new mojs.Burst({
+    radius:   { 0: 100 },
+    count:    20,
+    radius: 100,
+    scale:    { 1: 0 },
+    children: {
+      shape:      'polygon',
+      fill:       { 'purple' : 'purple' },
+      radius:     180,
+      rotate:      { 360: 0 },
+      duration:   1100,
+    },
+    top: 350,
+  });
+  
+
+
+
+/* -----------------------------------------
+fin timeline de Yanis 
+----------------------------------------- */
+
+const Yanistimeline = new mojs.Timeline({});
+
+Yanistimeline.append(burst1)
+
+
+
+
+new MojsPlayer({ add: Yanistimeline, isPlaying: true, });
 
 
 
@@ -18,7 +85,8 @@ const circle = new mojs.Shape({
 
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    new MojsPlayer({ add: circle, isPlaying: true, isRepeat: true});
-})
+
+
+
+
 
