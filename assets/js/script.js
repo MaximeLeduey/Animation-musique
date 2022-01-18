b3dfa69f51123128fa10d79b580dadf77e47caf2
Evan ROZEN <evanrozen3@gmail.com>
2022-01-18 08:27:50 +0100

    v6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,59 @@
 const circle1 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'blue',
-    x: 500,
+  parent: ".div-animated",
+  shape: "circle",
+  fill: "blue",
+  radius: { 10: 100 },
+  isYoyo: true,
+  x: 500,
+  duration: 2000,
 }).play();
-const circle2 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'yellow',
-    x: 400,
+const rectangle = new mojs.Shape({
+  parent: ".div-animated",
+  shape: "rect",
+  fill: "none",
+  stroke: "purple",
+  radius: 10,
+  strokeWidth: 20,
+  rotate: { [-180]: 0 },
+  duration: 800,
+  x: 400,
+})
+  .then({
+    strokeWidth: { 50: 0 },
+    stroke: { darkred: "yellow" },
+  })
+  .play();
+const polygon = new mojs.Shape({
+  parent: ".div-animated",
+  shape: "polygon",
+  points: 6,
+  fill: "green",
+  x: 300,
 }).play();
-const circle3 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'green',
-    x: 300,
+const rectangle2 = new mojs.Shape({
+  parent: ".div-animated",
+  shape: "circle",
+  fill: "red",
+  x: 200,
 }).play();
-const circle4 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'red',
-    x: 200,
-}).play();
-const circle5 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'gray',
-    x: 100,
+const star = new mojs.Shape({
+  parent: ".div-animated",
+  shape: "circle",
+  fill: "gray",
+  x: 100,
 }).play();
 const circle6 = new mojs.Shape({
-    parent: '.div-animated',
-    shape: 'circle',
-    fill: 'purple',
+  parent: ".div-animated",
+  shape: "circle",
+  fill: "purple",
 }).play();
 
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    // new MojsPlayer({ add: circle1, circle2, isPlaying: true, isRepeat: true});
-})
-
+document.addEventListener("DOMContentLoaded", (event) => {
+  new MojsPlayer({
+    add: circle1,
+    rectangle,
+    polygon,
+    isPlaying: true,
+    isRepeat: true,
+  });
+});
