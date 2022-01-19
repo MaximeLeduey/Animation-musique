7b66eca2d1538d4c66033fe6ea9a98abaed1c41b
Evan ROZEN <evanrozen3@gmail.com>
2022-01-19 10:32:32 +0100

    Evan  timeline v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 /* -------------------------------------------------------------------------------------------
 ----------------------------Début timeline de Evan-------------------------------------------- */
 
-
-
 // const circle1 = new mojs.Shape({
 //   parent: ".div-animated",
 //   shape: "circle",
@@ -23,21 +21,21 @@ const rect1 = new mojs.Shape({
   radius: { 10: 70 },
   strokeWidth: 20,
   rotate: { 0: 180 },
-  duration: 800,
-  speed: 3,
+  duration: 1000,
+  speed: 2,
   delay: 900,
   x: { 0: 500 },
   y: { 0: -200 },
   isYoyo: true,
-})
-  .then({
-    strokeWidth: { 50: 0 },
-    stroke: { yellow: "green" },
-    x: { 500: -200 },
-    y: { 0: 50 },
-    speed: 2,
-  })
-  .play();
+  // repeat : 2,
+}).then({
+  strokeWidth: { 50: 0 },
+  stroke: { yellow: "green" },
+  x: { 500: -200 },
+  y: { 0: 50 },
+  speed: 2,
+});
+// .play();
 
 const polygon1 = new mojs.Shape({
   parent: ".div-animated",
@@ -49,18 +47,18 @@ const polygon1 = new mojs.Shape({
   x: { 0: 500 },
   y: { 0: 200 },
   rotate: { [-180]: 0 },
-  duration: 1000,
-  speed: 2,
+  duration: 1500,
+  speed: 3,
   delay: 900,
-})
-  .then({
-    stroke: { blue: "purple" },
-    x: { 500: 200 },
-    y: { 200: -200 },
-    strokeWidth: { 100: 10 },
-    speed: 2,
-  })
-  .play();
+  // repeat : 2,
+}).then({
+  stroke: { blue: "purple" },
+  x: { 500: 200 },
+  y: { 200: -200 },
+  strokeWidth: { 100: 10 },
+  speed: 3,
+});
+// .play();
 
 class star extends mojs.CustomShape {
   getShape() {
@@ -73,63 +71,202 @@ const star1 = new mojs.Shape({
   shape: "star",
   fill: "yellow",
   x: { 0: -500 },
-  y: { 0: 200 },
+  y: { 0: 160 },
+  duration: 600,
   speed: 3,
   delay: 900,
 })
   .then({
     fill: "red",
-    speed: 2,
+    speed: 3,
     x: { 0: 70 },
     y: { 0: -50 },
   })
   .then({
     fill: "pink",
-    speed: 2,
+    speed: 3,
     x: { 70: 100 },
-    y: { 60: 200 },
+    y: { 60: 180 },
   })
   .then({
     fill: "lime",
-    speed: 2,
+    speed: 3,
     x: { 100: 40 },
     y: { 200: 80 },
   })
   .then({
     fill: "darkblue",
-    speed: 2,
+    speed: 3,
     x: { 40: 500 },
-    y: { 80: 150 },
+    y: { 80: 120 },
   })
   .then({
-    fill: "black",
-    speed: 2,
+    fill: "white",
+    speed: 3,
     x: { 500: 150 },
-    y: { 150: 30 },
+    y: { 120: -90 },
   })
   .then({
     fill: "yellow",
-    speed: 2,
+    speed: 3,
     x: { 150: -500 },
-    y: { 300: -100 },
-  })
+  });
 
-  .play();
+// .play();
 
-const burst = new mojs.Shape({
-  parent: ".div-animated",
-  fill: "brown",
+// const burst = new mojs.Shape({
+//   parent: ".div-animated",
+//   fill: "brown",
+//   x: { 0: -500 },
+//   y: { 0: -200 },
+//   radius : {0 : 100},
+//   count : 50,
+//   degree : 360,
+//   speed: 3,
+//   delay: 900,
+// }).play();
+
+const burst1 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
   x: { 0: -500 },
   y: { 0: -200 },
-  radius : {0 : 100},
-  count : 50,
-  degree : 360,
-  speed: 3,
+  children: {
+    fill: { red: "yellow" },
+    duration: 3000,
+  },
   delay: 900,
-}).play();
+  // repeat : 2,
+});
+
+const burst2 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: { 0: -500 },
+  y: { 0: 200 },
+  children: {
+    fill: { yellow: "blue" },
+    duration: 3000,
+  },
+  delay: 900,
+  // repeat : 2,
+});
+
+const burst3 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: { 0: 500 },
+  y: { 0: -200 },
+  children: {
+    fill: { blue: "pink" },
+    duration: 3000,
+  },
+  delay: 900,
+  // repeat : 2,
+});
+
+const burst4 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: { 0: 500 },
+  y: { 0: 200 },
+  children: {
+    fill: { pink: "black" },
+    duration: 3000,
+  },
+  delay: 900,
+  // repeat : 2,
+});
+
+const burst5 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: 400,
+  y: 180,
+  children: {
+    fill: "purple",
+    duration: 3000,
+  },
+  delay: 1000,
+  // repeat : 2,
+});
+
+const burst6 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: -400,
+  y: 180,
+  children: {
+    fill: "lime",
+    duration: 3000,
+  },
+  delay: 2000,
+  // repeat : 2,
+});
+
+const burst7 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: 400,
+  y: -180,
+  children: {
+    fill: "cyan",
+    duration: 3000,
+  },
+  delay: 3000,
+  // repeat : 2,
+});
+
+const burst8 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: 0,
+  y: 0,
+  children: {
+    fill: { red: "yellow" },
+    duration: 3000,
+  },
+  delay: 4000,
+  // repeat : 2,
+});
+
+const burst9 = new mojs.Burst({
+  radius: { 50: 200 },
+  count: 20,
+  opacity: { 1: 0 },
+  x: -400,
+  y: -180,
+  children: {
+    fill: "white",
+    duration: 3000,
+  },
+  delay: 5000,
+  // repeat : 2,
+});
 
 const Evantimeline = new mojs.Timeline();
-Evantimeline.add(rect1, polygon1, star1, burst);
+Evantimeline.add(
+  rect1,
+  polygon1,
+  star1,
+  burst1,
+  burst2,
+  burst3,
+  burst4,
+  burst5,
+  burst6,
+  burst7,
+  burst8,
+  burst9
+);
 
 document.addEventListener("DOMContentLoaded", (event) => {
   new MojsPlayer({
@@ -138,7 +275,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     isRepeat: true,
   });
 });
-/* -------------------------------------------------------------------------------------------
-----------------------------Fin timeline de Evan--------------------------------------------
----------------------------------------------------------------------------------------------- */
-
+/* ---------------------------------------------------------------------------------------------
+----------------------------  Fin timeline de Evan  --------------------------------------------
+------------------------------------------------------------------------------------------------ */
